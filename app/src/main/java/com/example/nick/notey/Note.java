bda6238b3c3b59f472ea39404059b9a4a10691f7
nick <nickawilkins@hotmail.com>
2014-11-05 10:12:06 -0800

    working on animations

--- app/src/main/java/com/example/nick/notey/Note.java
+++ app/src/main/java/com/example/nick/notey/Note.java
@@ -9,13 +9,17 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewAnimationUtils;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 
-public class Note extends Activity {
+public class Note extends Activity implements View.OnTouchListener {
 
     private static final String LIST_FRAGMENT_TAG = "list frag";
     private RecyclerView mRecyclerView;
@@ -23,12 +27,19 @@ public class Note extends Activity {
     private RecyclerView.LayoutManager mLayoutManager;
     private DataSource myDataSource;
     private String[] myDataSet;
+    private ViewGroup activeButton = null;
+    private ViewGroup buttonsContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.recycler_view);
-        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+        this.mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+
+        int buttonsSpacing = (int) getResources().getDimension(R.dimen.activity_horizontal_margin);
+        int buttonSize = (int) getResources().getDimension(R.dimen.activity_horizontal_margin);
+        Outline circularOutline = new Outline();
+        circularOutline.setOval(0, 0, buttonSize, buttonSize);
 
         mRecyclerView.setHasFixedSize(true);
         myDataSet = new String[] {"test string 1","test string 2"};
@@ -39,26 +50,17 @@ public class Note extends Activity {
         // specify an adapter (see also next example)
         mAdapter = new MyAdapter(myDataSet);
         mRecyclerView.setAdapter(mAdapter);
-/*
-        LinearLayout ll = (LinearLayout) findViewById(R.id.circle_layout);
+        LinearLayout ll = (LinearLayout) findViewById(R.id.buttonsContainer);
+        this.buttonsContainer = (ViewGroup) findViewById(R.id.buttonsContainer);
+        ViewGroup buttonHost = (ViewGroup) getLayoutInflater().inflate(R.layout.circle_view, buttonsContainer, false);
+        TextView button = (TextView) buttonHost.getChildAt(0);
 
-        LayoutInflater inflater = (LayoutInflater)getSystemService(this.LAYOUT_INFLATER_SERVICE);
+        button.setText("Test 1");
+        buttonHost.setOnTouchListener(this);
+        buttonsContainer.addView(buttonHost);
+        mLayoutManager.addView(buttonsContainer);
+        selectButton(((ViewGroup) buttonsContainer.getChildAt(0)), false);
 
-        View vi = inflater.inflate(R.layout.circle_view, null);
-
-        //View.inflate(this, R.layout.circle_view, null);
-
-        ImageButton plusBtn = (ImageButton) vi.findViewById(R.id.add_button);
-
-        ll.addView(vi);
-        //View.inflate(this, R.layout.recycler_view, ll);
-        /*
-        int diameter = getResources().getDimensionPixelSize(R.dimen.diameter);
-        Outline outline = new Outline();
-        outline.setOval(0, 0, diameter, diameter);
-        View addButton = v.findViewById(R.id.add_button);
-        addButton.setOutline(outline);
-        addButton.setClipToOutline(true);*/
     }
 
     @Override
@@ -98,5 +100,42 @@ public class Note extends Activity {
                             LIST_FRAGMENT_TAG
                     ).addToBackStack(null).commit();
         }
+    }
+
+    private void selectButton(ViewGroup buttonHost, boolean reveal, int startX, int startY) {
+        if (buttonHost == activeButton) {
+            return;
+        }
+        if (activeButton != null) {
+            activeButton.setSelected(false);
+            activeButton = null;
+        }
+        activeButton = buttonHost;
+        activeButton.setSelected(true);
+        View button = activeButton.getChildAt(0);
+        if (reveal) {
+            ViewAnimationUtils.createCircularReveal(button,
+                    startX,
+                    startY,
+                    0,
+                    button.getHeight()).start();
+        }
+    }
+
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+        switch (motionEvent.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                ((ViewGroup) view).getChildAt(0).getBackground().setHotspot(motionEvent.getX(), motionEvent.getY());
+                break;
+            case MotionEvent.ACTION_UP:
+                selectButton((ViewGroup) view, true, (int) motionEvent.getX(), (int) motionEvent.getY());
+                break;
+        }
+        return false;
+    }
+
+    private void selectButton(ViewGroup buttonHost, boolean reveal) {
+        selectButton(buttonHost, reveal, buttonHost.getWidth(), buttonHost.getHeight());
     }
 }
